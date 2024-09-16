890eb2fc3f269c7e7e58649d77bf3355c2e7bc10
Damião <jose.faustino@hfa.mil.br>
2024-09-16 13:46:34 -0300

    finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
-  <button @click="start" :disabled="isPlaying">play</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <button class="botao" @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <!-- <p v-if="showResults">Reaction time: {{ score }} ms</p> -->
+  <Result v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 
 export default {
   name: 'App',
   components: {
-    Block
+    Block, Result
   },
   data(){
     return{
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods:{
     start(){
       this.delay = 2000 + Math.random() * 5000,
       this.isPlaying = true,
-      console.log(this.delay)
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
@@ -36,5 +46,21 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+
+.botao{
+  width: 100px;
+  height: 30px;
+  background: #2d30f3;
+  color: white;
+  border-radius: 5px;
+  border: none;
+}
+.botao:hover{
+  background: #4446ee;
+  color: white;
+  border: 1px white;
+  cursor: pointer;
+  font-weight: bold;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="block red">
+    <h3>Your result is : </h3>
+    <p>{{ score }} ms</p>
+    <p v-if="result">{{ result }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props : [
+        'score'
+    ],
+    data(){
+        return{
+            result : null
+        }
+    },
+   
+}
+</script>
+
+<style>
+    
+</style>
